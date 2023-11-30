0ba94eb28742779785e2078035d1279135d8b163
Mykhailo Volontyr <boommmeran@gmail.com>
2023-11-30 13:35:37 +0200

    done hw

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
-const key = new Key();
+class Key {
+	private signature: number = Math.random();
 
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {}
+
+	getKey(): Key {
+		return this.key;
+	}
+}
+
+abstract class House {
+		protected door: boolean = false
+		protected key: Key
+		protected tenants: Person[] = []
+
+	comeIn(person: Person): void {
+		if (this.door) {
+      this.tenants.push(person);
+		}
+	}
+
+	abstract openDoor(currentKey: Key): void;
+}
+
+class MyHouse extends House {
+	constructor(key: Key) {
+		super();
+		this.key = key;
+	}
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
